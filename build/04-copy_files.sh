2fb06b10cdde2b980f405e35b6c3b161ac9c5ef6
Christoph Hummer <christophhummer@gmail.com>
2023-09-17 08:08:15 +0200

    Update 04-copy_files.sh

--- build/04-copy_files.sh
+++ build/04-copy_files.sh
@@ -2,5 +2,5 @@
 cd /tmp/build
 chmod 755 $(ls -1 /tmp/build/ | grep -v "^[0-9][0-9]-")
 cp /tmp/build/homeassistant@homeassistant.service /etc/systemd/system/homeassistant@homeassistant.service
-cp /tmp/build/update-ha.sh /root/update-ha.sh
+cp /tmp/build/update-ha.sh /home/homeassistant/update-ha.sh
 cp /tmp/build/generate_ssh_keys.sh /etc/init.d/generate_ssh_keys.sh
