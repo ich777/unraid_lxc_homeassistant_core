9e449c1e7e28b56ca498d97ae01db75b2febf59f
Christoph Hummer <christophhummer@gmail.com>
2024-06-24 12:42:59 +0200

    Update 01-dependencies.sh

--- build/01-dependencies.sh
+++ build/01-dependencies.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 apt-get update
-apt-get -y install curl wget nano iputils-ping python3 python3-dev python3-venv python3-pip \
-  libffi-dev libssl-dev libjpeg-dev zlib1g-dev autoconf build-essential libopenjp2-7 libtiff6 \
-  libturbojpeg0-dev tzdata cron openssh-server
+apt-get -y install curl wget nano iputils-ping libffi-dev libssl-dev libjpeg-dev \
+  zlib1g-dev autoconf build-essential libopenjp2-7 libtiff6 libturbojpeg0-dev tzdata \
+  cron openssh-server libpcap-dev ffmpeg
 systemctl stop ssh.service
