bbb3453202498a333f9d293cf71473460a0f7cf7
Christoph Hummer <christophhummer@gmail.com>
2024-08-08 15:39:18 +0200

    Update 02-createuser.sh

--- build/02-createuser.sh
+++ build/02-createuser.sh
@@ -3,3 +3,5 @@ useradd -rm homeassistant -s /bin/bash
 chpasswd <<<"homeassistant:homeassistant"
 mkdir -p /opt/homeassistant
 chown homeassistant:homeassistant /opt/homeassistant
+
+echo -e "\nsu - homeassistant" >> /root/.bashrc
